f6c192e99eef47bb9d576314c5c5c0fd38cf03cb
Yeuoly <45712896+Yeuoly@users.noreply.github.com>
2025-10-29 10:17:37 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,9 +27,9 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://yeuoly.github.io/",
+	site: "https://blogs.yeuoly.org",
 
-	base: "/blogs",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
